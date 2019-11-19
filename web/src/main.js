293437cb1b44acf34f7ef7c73554b3ea0cacc037
戴肖 <1032526395@qq.com>
2019-11-19 14:48:28 +0800

    提交

--- web/src/main.js
+++ web/src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import store from './store'
 import './registerServiceWorker'
 import './plugins/element.js'
-
+import "./assets/stylus/index.styl"
 Vue.config.productionTip = false
 
 new Vue({
